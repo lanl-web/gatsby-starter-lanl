070e6dac864663c5722228b2fdc5633abe5fcc91
Austin Bentley <austin.bentley@lanl.gov>
2021-11-09 10:59:02 -0700

    refactor: simplify components

--- src/templates/article/StandardImage/index.js
+++
@@ -1,1 +0,0 @@
-export { StandardImage } from './StandardImage';
--- src/templates/article/index.js
+++ src/templates/article/index.js
@@ -2,11 +2,17 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { graphql } from 'gatsby';
 
-import { Banner, BreadCrumbs, SocialBlock } from '@lanl-web/lanl-components';
+import {
+  Banner,
+  BreadCrumbs,
+  SocialBlock,
+  ContentBody,
+  ContentIntroduction,
+  ContentTitle,
+} from '@lanl-web/lanl-components';
 
-import { StandardContent } from './StandardContent';
-import { StandardHeading } from './StandardHeading';
-import { StandardImage } from './StandardImage';
+import { ArticleImage } from './ArticleImage';
+import { replaceElements } from '../../utils';
 
 import Layout from '../../components/layout';
 import SEO from '../../components/seo';
@@ -17,11 +23,12 @@ const ArticleTemplate = ({ location, data }) => {
   const name = 'name';
   const marqueeImage =
     'https://cdn.lanl.gov/5f81937f-4b68-42c9-a57b-6cd2ee87cd28.jpeg';
-  const introduction = 'introduction';
-  const title = 'title';
+  const introduction =
+    'Without adequate infrastructure, scholarly artifacts will vanish from the web in much the same way and with similar frequency &ldquo;regular&rdquo; web resources do.';
+  const title = 'Scholarly Orphans';
   const shareLink = '';
   const page = {
-    body: '<p>Body text here</p>',
+    body: '<h1>This is a title</h1><h2>This is a subhead</h2><p>Some body text with an image from the CMS</p><img height="235" width="350" alt="51063015912 Ce92a87fd2 O" src="https://cdn.lanl.gov/5f81937f-4b68-42c9-a57b-6cd2ee87cd28.jpeg" class="image-left block-left"><p>More test</p>',
     feature_image: {
       filename_disk: '5e5018f8-a0ff-4fda-9986-3e27ccac12cf.jpeg',
       description: 'This is a test caption',
@@ -44,13 +51,14 @@ const ArticleTemplate = ({ location, data }) => {
             location={location}
             homeName='Sustainability'
           />
-          <StandardHeading introduction={introduction} title={title} />
+          <ContentTitle>{title}</ContentTitle>
+          <ContentIntroduction>{introduction}</ContentIntroduction>
         </div>
 
         <div className='main'>
           <div className='main-article'>
-            <StandardImage story={page} />
-            <StandardContent story={page} />
+            <ArticleImage story={page} />
+            <ContentBody body={replaceElements(page?.body)} className='body' />
           </div>
 
           <div className='main-links'>
